6fea11fbf1831539bd3e8d3e03176bb8d36b78fb
zimeng <28741344+zimenglyu@users.noreply.github.com>
2024-02-15 12:55:47 -0500

    added running scripts

--- predict_run_0.sh
+++ predict_run_0.sh
@@ -1,7 +1,7 @@
-train_spectra="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10/Cyclone_10_202303_202105_202209_spectra.csv"
-lab_result_spectra="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10/Cyclone_10_202303_202105_202209_lab_results_spectra.csv"
-lab_result_label="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10/Cyclone_10_202303_202105_202209_lab_results.csv"
-test_data="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10/Cyclone_10_202303_202105_202209_lab_results_spectra.csv"
+# train_spectra="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10/Cyclone_10_202303_202105_202209_spectra.csv"
+# lab_result_spectra="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10/Cyclone_10_202303_202105_202209_lab_results_spectra.csv"
+# lab_result_label="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10/Cyclone_10_202303_202105_202209_lab_results.csv"
+# test_data="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10/Cyclone_10_202303_202105_202209_lab_results_spectra.csv"
 # test_data="/Users/zimenglyu/Documents/datasets/microbeam/PPM/Cyclone_10_March_Sampling_Spectra.csv"
 
 # train_spectra="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_3/Cyclone_3_combined_spectra.csv"
@@ -9,17 +9,26 @@ test_data="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_10
 # lab_result_label="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_3/Cyclone_3_combined_lab_results.csv"
 # test_data="/Users/zimenglyu/Documents/datasets/microbeam/PPM/combined/cyclone_3/cyclone_3_lab_results_spectra.csv"
 
-data_set="combined"
+# data_set="combined"
+
+train_spectra="/Users/zimenglyu/Documents/datasets/SOM_flight/flight_53438.csv"
+lab_result_spectra="/Users/zimenglyu/Documents/datasets/SOM_flight/flight_53438_5.csv"
+lab_result_label="/Users/zimenglyu/Documents/datasets/SOM_flight/flight_53438_5_label.csv"
+test_data="/Users/zimenglyu/Documents/datasets/SOM_flight/flight_53438_5.csv"
+# test_data="/Users/zimenglyu/Documents/datasets/microbeam/PPM/Cyclone_10_March_Sampling_Spectra.csv"
+
+data_set="flight"
+
 fold=0
 method=weighted
-epoch=40000
+epoch=10000
 som_path="/Users/zimenglyu/Documents/code/git/susi/som_results/combined/20_20/combined_20_20_40000_neighbor_5.pickle"
 
 for epoch in 20000 
 do
-    for n in 30
+    for n in 5 7 10
     do
-    folder_name="SOM_Result_NEW/$data_set/$n"  # Specify the folder path here
+    folder_name="SOM_Flight_5/$data_set/$n"  # Specify the folder path here
         if [ ! -d "$folder_name" ]; then
         echo "Creating folder: $folder_name"
         mkdir -p "$folder_name"
@@ -27,16 +36,16 @@ do
         echo "Folder already exists: $folder_name"
         fi
 
-        for neighbor in 3 # not run yet
+        for neighbor in 3 5 7 # not run yet
         do
             for norm_method in "minmax" "standard" "robust"
             do
-                for fold in 0 
+                for fold in 0 1 2 3 4 5 6 7 8 9
                 do
                     python SOM_Predictor.py --train_spectra $train_spectra \
                     --lab_result_spectra $lab_result_spectra \
                     --lab_result_label $lab_result_label \
-                    --estimate_method  "average" "random" \
+                    --estimate_method  "vote" "weighted" \
                     --testing_path $test_data \
                     --norm_method $norm_method \
                     --num_neighbors $neighbor \
